98c7aae77fe89addbb1e1174eea07d3c09051ffc
{fujitanao} <fujitanao.rmt@gmail.com>
2022-10-26 11:25:20 +0900

    Initial Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from "next/link"
 import styles from '../styles/Home.module.scss'
 
-const ReadAllItem: NextPage = (props) => {
+const ReadAllItem = (props) => {
 
   return (
     <div className={styles.container}>
