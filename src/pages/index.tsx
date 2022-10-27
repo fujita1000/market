77a39c6dc04031faba20d5f865a9848efdc9facf
{fujitanao} <fujitanao.rmt@gmail.com>
2022-10-28 03:00:13 +0900

    Initial Commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,34 @@
+import { gsap } from 'gsap';
 import type { NextPage, GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from "next/link"
+
+import { useEffect } from 'react';
 import styles from '../styles/Home.module.scss'
 import {ReadAllDataType} from "@/utils/types"
 
 
-const ReadAllItem: NextPage<ReadAllDataType> = (props) => {
 
+
+
+const ReadAllItem: NextPage<ReadAllDataType> = (props) => {
+  useEffect(() => {
+    gsap.set('#Card', {
+      duration: 2,
+  x:-500
+    });
+gsap.to("#Card",{
+  duration: 2,
+  opacity:1,
+  x:0
+})
+
+  }, []);
   return (
     <div className={styles.container}>
       <h1>本田マーケット</h1>
-      <div className={styles.Card_container}>
+      <div className={styles.Card_container} id="Card">
         {props.allItems.map((item) => (
           <Link href={`/item/${item._id}`} key={item._id}>
             <div className={styles.Card}>
